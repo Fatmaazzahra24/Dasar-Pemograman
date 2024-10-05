c9131c0c9ff94f5a76be46b8921bc60fca85ef63
Fatmaazzahra24 <azzahrafatma727@gmail.com>
2024-10-05 20:41:19 +0700

    modifikasi1

--- jobsheet6/pemilihan2percobaan1.java
+++ jobsheet6/pemilihan2percobaan1.java
@@ -8,10 +8,18 @@ public class pemilihan2percobaan1 {
         System.out.println("masukkan tahun : ");
         int tahun = input05.nextInt();
 
-        if ((tahun % 4) == 0){
-            if ((tahun%100)!=0)
-            System.out.println("Tahun Kabisat");
-        } else 
+        if (tahun % 4 == 0) {
+            if (tahun % 100 == 0) {
+                if (tahun % 400 == 0) {
+                    System.out.println("Tahun Kabisat");
+                } else {
+                    System.out.println("Bukan Tahun Kabisat");
+                }
+            } else {
+                System.out.println("Tahun Kabisat");
+            }
+        } else {
             System.out.println("Bukan Tahun Kabisat");
         }
+    }
 }

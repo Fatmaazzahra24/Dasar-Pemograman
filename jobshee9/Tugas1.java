469d848f509062e37a8573f9d9f2d09db329c0bf
Fatmaazzahra24 <azzahrafatma727@gmail.com>
2024-11-21 09:09:56 +0700

    tugas jobsheet

--- jobshee9/Tugas1.java
+++ jobshee9/Tugas1.java
@@ -1,3 +1,38 @@
+import java.util.Scanner;
+
 public class Tugas1 {
+
+    public static void main(String[] args) {
+    Scanner fatma=new Scanner(System.in);
     
+    System.out.print("Masukkan  banyaknya nilai Mahasiswa yang diinput :");
+    int JmlMhs =fatma.nextInt();
+
+    int [] NilaiMhs = new int [JmlMhs];
+    double total=0, ratarata ;
+    int max =Integer.MIN_VALUE;
+    int min =Integer.MAX_VALUE;
+
+    for (int i = 0; i < JmlMhs; i++) {
+        System.out.print("Masukkan nilai Mahasiswa ke-"+(i+1)+ " : ");
+        NilaiMhs[i]=fatma.nextInt();
+        total +=NilaiMhs[i];
+        if (NilaiMhs[i] > max) {
+            max = NilaiMhs[i];
+        } else if (NilaiMhs[i] < min) {
+            min = NilaiMhs[i];
+        }
+    }
+    
+    System.out.println("===============================================");
+
+    for ( int i = 0; i < JmlMhs;i++) {
+        System.out.println("Nilai Mahasiswa ke-" +(i+1)+ " : "+NilaiMhs[i]);
+    }
+    ratarata = total/NilaiMhs.length;
+    System.out.println("Rata rata nilai Mahasiswa adalah :" +ratarata);
+    System.out.println("Nilai maksimal adalah : " +max );
+    System.out.println("Nilai minimal adalah : " +min );  
+    
+}
 }
---
+++ jobshee9/Tugas3.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Tugas3 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        String[] menu = {"Nasi Goreng", "Mie Goreng", "Roti Bakar","Kentang Goreng", 
+                        "Teh Tarik", "Cappucino", "Chocolate Ice"};
+
+        System.out.print("Masukkan nama makanan yang ingin dicari: ");
+        String makananyangdicari = scanner.nextLine();
+
+
+        boolean tersedia = false;
+        for (int i = 0; i < menu.length; i++) {
+            if (menu[i].equalsIgnoreCase(makananyangdicari)) {
+                tersedia = true;
+                break;
+            }
+        }
+
+        if (tersedia) {
+            System.out.println( makananyangdicari + " tersedia di menu.");
+        } else {
+            System.out.println( makananyangdicari + " tidak ada di menu.");
+        }
+    }
+}

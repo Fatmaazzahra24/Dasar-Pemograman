962fdd616cfb286b72075ec47f24fd7844aa06a7
Fatmaazzahra24 <azzahrafatma727@gmail.com>
2024-11-24 20:15:21 +0700

    modif

--- jobhseet10/Numbers05.java
+++ jobhseet10/Numbers05.java
@@ -1,3 +1,6 @@
+
+import java.util.Arrays;
+
 public class Numbers05 {
     public static void main(String[] args) {
         
@@ -6,6 +9,14 @@ public class Numbers05 {
     myNumbers [0]= new int [5];
     myNumbers [1]= new int [3];
     myNumbers [2]= new int [1];
+
+    for (int i = 0; i < myNumbers.length; i++) {
+       System.out.println(Arrays.toString(myNumbers[i]));
+    }
+
+    for (int i = 0; i < myNumbers.length; i++) {
+        System.out.println("panjang baris ke-" + (i+1) + " : " +myNumbers[i].length);
+    }
     
     }
     
--- jobhseet10/SIAKAD05.java
+++ jobhseet10/SIAKAD05.java
@@ -3,30 +3,37 @@ import java.util.Scanner;
 public class SIAKAD05 {
     public static void main(String[] args) {
     Scanner fatma=new Scanner(System.in);
-    int [][] nilai = new int [4][3];
 
-    for (int i = 0; i < nilai.length; i++) {
+    System.out.print("Masukkan jumlah Mahasiswa :");
+    int JmlMhs = fatma.nextInt();
+
+    System.out.print("Masukkan jumlah mata kuliah :");
+    int JmlMatkul=fatma.nextInt();
+
+    int [][] nilai = new int [JmlMhs][JmlMatkul];
+
+    for (int i = 0; i < JmlMhs; i++) {
         System.out.println("input nilai mahasiswa ke-" + (i+1));
         double totalPerSiswa = 0;
 
-        for (int j = 0; j < nilai[i].length; j++) {
+        for (int j = 0; j < JmlMatkul; j++) {
             System.out.print("nilai mata kuliah " +  (j+1)  + ":");
             nilai [i][j] = fatma.nextInt(); 
             totalPerSiswa += nilai [i][j];
         }
-        System.out.print("nilai rata rata : " + totalPerSiswa/3);
+        System.out.println("nilai rata rata : " + totalPerSiswa/JmlMatkul);
     }
 
     System.out.println("\n===============================================");
     System.out.println(" Rata rata nilai setiap mata kuliah :");
     
-    for (int j = 0; j < 3; j++) {
+    for (int j = 0; j < JmlMatkul; j++) {
         double totalPerMatkul = 0;
 
-        for (int i = 0; i < 4; i++) {
+        for (int i = 0; i < JmlMhs; i++) {
             totalPerMatkul += nilai [i][j];
         }
-        System.out.println("Mata Kuliah " +(j+1) + " :" +totalPerMatkul/4);
+        System.out.println("Mata Kuliah " +(j+1) + " :" +totalPerMatkul/JmlMhs);
     }
 
     
